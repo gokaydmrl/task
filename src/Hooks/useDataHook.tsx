464ee2cd8ef2134c4401ecc8614b59fd07d3c5ae
gokaydmrl <gokaydemirel96@gmail.com>
2023-01-18 15:32:31 +0300

    bug

--- src/Hooks/useDataHook.tsx
+++ src/Hooks/useDataHook.tsx
@@ -1,75 +1,74 @@
 import { useState, useEffect, useRef } from "react";
 import * as mqtt from "mqtt/dist/mqtt.min";
-import { IClientOptions } from "mqtt/dist/mqtt.min";
+import { IClientOptions, IClientSubscribeOptions } from "mqtt/dist/mqtt.min";
 
 const useDataHook = () => {
   const clientId = "digiterra-coding-task-1";
   const hostt = "ws://138.68.8.53:8000/mqtt";
   const options = {
+    
     keepalive: 60000000,
     clientId: clientId,
     protocolId: "MQTT",
     protocolVersion: 4,
     clean: true,
-    reconnectPeriod: 5000,
+    reconnectPeriod: 1000,
     connectTimeout: 30 * 1000,
     will: {
       topic: "WillMsg",
-      payload: "Connection Closed abnormally..!",
+      payload: "Cnnection Closed abnormally..!",
       qos: 0,
       retain: false,
     },
     rejectUnauthorized: false,
   };
 
-  const [client, setClient] = useState<any>(null);
+  // const [client, setClient] = useState<any>(null);
   const [connectionStatus, setConnectionStatus] = useState<boolean | null>(null);
   const [payload, setPayload] = useState<any>(null);
+console.log(payload);
 
   const subscriptionn = {
     topic: "$SYS",
-    qos: 1,
+    qos: 0,
   };
-  const mqttSub = (subscription: any) => {
-    if (client) {
-      const { topic, qos } = subscription;
-      client.subscribe(topic, qos, (error: any) => {
-        if (error) {
-          console.log("Subscrie to topics error", error);
-          return;
-        }
-        console.log("toopic", topic);
-      });
-    }
-  };
+  // const mqttSub = (subscription: any) => {
+  //   const { topic, qos } = subscription;
+  //   client.subscribe(topic, qos, (error: any) => {
+  //     if (error) {
+  //       console.log("Subscie to topics error", error);
+  //       return;
+  //     }
+  //     console.log("toopic", topic);
+  //   });
+  // };
 
-  console.log("clieent", client);
+  // const makeConnection = () => {
+  //   mqtt.connect(hostt, options as IClientOptions);
+  //   setClient(mqtt.connect(hostt, options as IClientOptions));
+  // };
+
+  // console.log("clieent", client);
 
   useEffect(() => {
-    const makeConnection = () => {
-      mqtt.connect(hostt, options as IClientOptions);
-      setClient(mqtt.connect(hostt, options as IClientOptions));
-    };
-    makeConnection();
-    mqttSub(subscriptionn);
-    if (client) {
-      console.log(client.connected);
-      client.on("connect", () => {
-        console.log("Connected");
-      });
-      client.on("error", (err: any) => {
-        console.log("Connectiion errror: ", err);
-      });
-      client.on("reconnect", () => {
-        console.log("Recconnectng");
-      });
-      client.on("message", (payload: any) => {
-        console.log("payload", payload);
-        setPayload(payload);
-      });
-    } else {
-      console.log("no cli");
-    }
+    mqtt.connect(hostt, options as IClientOptions);
+    const client = mqtt.connect(hostt, options);
+    client.subscribe(subscriptionn.topic, 0, (err) => console.log("subscribe error", err));
+    // mqttSub(subscriptionn);
+    console.log(client);
+    client.on("connect", () => {
+      console.log("Connected");
+    });
+    client.on("error", (err: any) => {
+      console.log("Connectiion errror: ", err);
+    });
+    client.on("reconnect", () => {
+      console.log("Reconnectng");
+    });
+    client.on("message", (payload: any) => {
+      console.log("payload", payload);
+      setPayload(payload);
+    });
   }, []);
 
   interface Host {
