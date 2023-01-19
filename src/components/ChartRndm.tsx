39df036a181348ab06a54681c0be2812000b2760
gokaydmrl <gokaydemirel96@gmail.com>
2023-01-19 23:44:14 +0300

    pie

--- src/components/ChartRndm.tsx
+++ src/components/ChartRndm.tsx
@@ -4,7 +4,7 @@ import ReactEcharts from "echarts-for-react";
 function ChartRndm({ data, type, time }: { data: number[]; type: string; time: string[] }) {
   const lastInd = data.length - 1;
   if (type === "pie") {
-    data = data.slice(lastInd - 2);
+    data = data.slice(lastInd - 1);
   }
   const options = {
     grid: { top: 8, right: 8, bottom: 24, left: 36 },
@@ -27,6 +27,28 @@ function ChartRndm({ data, type, time }: { data: number[]; type: string; time: s
     },
   };
 
-  return <ReactEcharts option={options} />;
+  return type === "pie" ? (
+    <div
+      style={{
+        display: "flex",
+      }}>
+      <div
+        style={{
+          width: "20rem",
+          height: "auto",
+        }}>
+        <ReactEcharts option={options} />
+      </div>
+      <div
+        style={{
+          display: "grid",
+        }}>
+        <p>current data: {data[1]}</p>
+        <p>last data: {data[0]} </p>
+      </div>
+    </div>
+  ) : (
+    <ReactEcharts option={options} />
+  );
 }
 export default ChartRndm;
