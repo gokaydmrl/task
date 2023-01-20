322fd4b003e9cf417c55f536a83f98128781d5d8
gokaydmrl <gokaydemirel96@gmail.com>
2023-01-20 14:00:11 +0300

    task

--- src/Hooks/useRandomDataHook.tsx
+++ src/Hooks/useRandomDataHook.tsx
@@ -3,7 +3,7 @@ import * as mqtt from "mqtt/dist/mqtt.min";
 import { IClientOptions, IClientSubscribeOptions, QoS } from "mqtt/dist/mqtt.min";
 
 const useRandomDataHook = () => {
-/*
+  /*
 Since I didn't reach the mqtt metrics, i created a hook 
 that returns random numbers every 3 seconds.
 Then, I update the data using spread operator
@@ -14,7 +14,7 @@ the changes inside an array doesnt update the array.
   const reachedAt: () => string = () => new Date().toLocaleTimeString("tr-TR");
   const newRandomData: number = Math.floor(Math.random() * 200);
   const [times, setTimes] = useState<string[]>([reachedAt()]);
-  const [data, setData] = useState<number[]>([newRandomData]);
+  const [data, setData] = useState<number[]>([Math.floor(Math.random() * 200)]);
 
   useEffect(() => {
     const timer = setInterval(() => {
